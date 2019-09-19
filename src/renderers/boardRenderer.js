8278d0ebf1ba4e17ae70047eebab111663513af5
Thunder Rice <thunderriceman@gmail.com>
2019-09-19 16:59:12 +0100

    eslint

--- src/renderers/boardRenderer.js
+++ src/renderers/boardRenderer.js
@@ -1,23 +1,23 @@
 export default class BoardRenderer {
 
-    render(container, board) {
-        let size = board.getSize();
+  render(container, board) {
+    let size = board.getSize();
 
-        for (let i = 0; i < size; i++) {
-            let row = document.createElement("div");
-            row.className = "row";
-            container.appendChild(row);
-            for (let j = 0; j < size; j++) {
-                let cell = document.createElement("div");
-                cell.className = "cell";
+    for (let i = 0; i < size; i++) {
+      let row = document.createElement("div");
+      row.className = "row";
+      container.appendChild(row);
+      for (let j = 0; j < size; j++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
 
-                if (board.getCell(i, j) !== undefined) {
-                    cell.innerText = board.getCell(i, j);
-                }
-
-                row.appendChild(cell);
-            }
+        if (board.getCell(i, j) !== undefined) {
+          cell.innerText = board.getCell(i, j);
         }
 
+        row.appendChild(cell);
+      }
     }
+
+  }
 }
